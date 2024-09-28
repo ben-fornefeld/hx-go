42e891a8e2161a8f668e1ac1f2d0dbf210cf819e
Ben Fornefeld <ben.fornefeld@gmail.com>
2024-09-28 16:45:44 +0200

    :wrench: add alpine js via bundle for client state only

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,3 +1,9 @@
+// INITIALIZE BASE
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+
+Alpine.start();
+
 // simply use your node_modules here or in any other js file that is exported here
 // e.g. import "./hello-world.js";
 
